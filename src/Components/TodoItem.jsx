a31ae8ee846a9c362269346b2e6eaa8ad05c7b2b
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-01-24 17:14:49 +0000

    replaced the complete button with a checkbox - it's cooler

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -3,7 +3,7 @@ import React from "react"
 export const Item = ({ item, removeItem, completeItem }) => {
   return (
     <li className={ item.complete ? "complete" : "incomplete" }>
-      <button onClick={ completeItem }>Complete</button>
+      <input type="checkbox" onChange={ completeItem }/>
       <p>{item.value}</p><button onClick={ removeItem }>Remove</button>
     </li>
   )
