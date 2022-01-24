483175d218c058b0241996854bd02c92246e3b00
Alex Wayland <89842550+web-lynx@users.noreply.github.com>
2022-01-24 17:54:49 +0000

    Very basic CSS - text strikethrough & some flexbox

--- src/Components/TodoItem.jsx
+++ src/Components/TodoItem.jsx
@@ -4,7 +4,9 @@ export const Item = ({ item, removeItem, completeItem }) => {
   return (
     <li className={ item.complete ? "complete" : "incomplete" }>
       <input type="checkbox" onChange={ completeItem }/>
-      <p>{item.value}</p><button onClick={ removeItem }>Remove</button>
+      <p className={ item.complete ? "complete" : "incomplete" }>{item.value}</p><button onClick={ removeItem }>Remove</button>
     </li>
   )
 }
+
+// className={ item.complete ? "complete" : "incomplete" }
